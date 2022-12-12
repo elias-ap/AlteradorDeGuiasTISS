7669e0d75de64d1be0527825872b9b0ae6adbf03
Elias Araujo Pedroso <elias.ap.pro@gmail.com>
2022-12-12 13:53:20 -0300

    Update project
    Created GUI
    Save funcionalities

--- automatic_alteration_branch1.py
+++ automatic_alteration_branch1.py
@@ -1,12 +1,16 @@
 # IMPORTS
-import hashlib
 import xml.etree.ElementTree as ET
+import os
 import pandas as PD
+import hashlib
 import tkinter as ttk
 from tkinter import filedialog as fd
 from tkinter import messagebox as mb
 
 """ FUNCTIONS THAT WILL BE USED BY SOFTWARE """
+
+global ans_prefix
+ans_prefix = {'ans': 'http://www.ans.gov.br/padroes/tiss/schemas'}  # SET TAG PREFIX USED AS DEFAULT BY TISS GUIDES
 
 
 def returnReviewLine(list, mode):
@@ -65,26 +69,27 @@ def alterTableType():
 
 
 def alterProcedureCode():
-    altered_data = 'procedure code'
-
     tag_procedure_code = procedure_data.find('ans:codigoProcedimento', ans_prefix)
     if new_procedure_code != '' and new_procedure_code != tag_procedure_code.text:
         tag_procedure_code.text = tag_procedure_code.text.replace(str(procedure_code), str(new_procedure_code))
+        altered_data = 'procedure code'
         generateAlterationLog(altered_data, procedure_code, new_procedure_code)
 
 
-def alterUnityMeasure():
-    altered_data = 'unity measure'
-    tag_unity_measure = procedure_data.find('ans:unidadeMedida', ans_prefix)
+########################################################################################################################
 
+def alterUnityMeasure():
+    tag_unity_measure = procedure_data.find('ans:unidadeMedida', ans_prefix)
     if new_unity_measure != '' and new_unity_measure != tag_unity_measure.text:
         tag_unity_measure.text = tag_unity_measure.text.replace(str(unity_measure), str(new_unity_measure)).rjust(
             len(tag_unity_measure.text), '0')
+        altered_data = 'unity measure'
         generateAlterationLog(altered_data, unity_measure, new_unity_measure)
 
 
+########################################################################################################################
+
 def alterValue():
-    altered_data = 'unitary value'
     unitary_value_tag = procedure_data.find('ans:valorUnitario', ans_prefix)
     procedure_total_value_tag = procedure_data.find('ans:valorTotal', ans_prefix)
     current_procedure_total_value = procedure_total_value_tag.text
@@ -98,21 +103,23 @@ def alterValue():
         else:
             value_difference = f'{float(procedure_total_value_tag.text) - float(current_procedure_total_value):.2f}'
 
+    def recalculateAllTotalValues(valueDifference):
+        account_total_values_tag = acnt.find('ans:valorTotal', ans_prefix)
+        general_total_values_tag = account_total_values_tag.find('ans:valorTotalGeral', ans_prefix)
+        wasRecalculated = False
+        for total_value in account_total_values_tag:
+            if wasRecalculated == False:
+                if total_value.text > valueDifference:
+                    total_value.text = f'{float(total_value.text) + float(valueDifference):.2f}'
+                    general_total_values_tag.text = f'{float(general_total_values_tag.text) + float(valueDifference):.2f}'
+                    wasRecalculated = True
+
         recalculateAllTotalValues(value_difference)
+        altered_data = 'unitary value'
         generateAlterationLog(altered_data, unitary_value, new_unitary_value)
 
 
-def recalculateAllTotalValues(valueDifference):
-    account_total_values_tag = cnt.find('ans:valorTotal', ans_prefix)
-    general_total_values_tag = account_total_values_tag.find('ans:valorTotalGeral', ans_prefix)
-    wasRecalculated = False
-    for total_value in account_total_values_tag:
-        if wasRecalculated == False:
-            if total_value.text > valueDifference:
-                total_value.text = f'{float(total_value.text) + float(valueDifference):.2f}'
-                general_total_values_tag.text = f'{float(general_total_values_tag.text) + float(valueDifference):.2f}'
-                wasRecalculated = True
-
+########################################################################################################################
 
 def generateAlterationLog(altered_data, old_value, new_value):
     old = old_value
@@ -122,6 +129,8 @@ def generateAlterationLog(altered_data, old_value, new_value):
     else:
         return print(f'Procedure:{procedure_code} {altered_data}:{old} altered for:{new}')
 
+
+########################################################################################################################
 
 def saveGuide():
     def removeHashTextFromGuide(root_tag):
@@ -149,90 +158,80 @@ def saveGuide():
 
     cancel()
     saveGuide_button['state'] = 'disabled'
+    mb.showinfo(message='Arquivo salvo!')
 
 
-ans_prefix = {'ans': 'http://www.ans.gov.br/padroes/tiss/schemas'}  # SET TAG PREFIX USED AS DEFAULT BY TISS GUIDES
-source_folder_path = 'sources'
-p = source_folder_path
-reviews_list = []
+########################################################################################################################
 
-
-def windowConfigure():
+def createGui():
+    # Create main Window
+    window = ttk.Tk()
     window.title('Alterador de guias TISS')
     window.geometry('400x250')
     window.eval('tk::PlaceWindow . center')
+    window.maxsize(width=400, height=250)
 
+    #  Create button Frame
+    button_frame = ttk.Frame(window)  # , background='blue')
+    button_frame.pack(side='bottom', pady=12, padx=100)
 
-def createButtons():
+    button_frame2 = ttk.Frame(window)  # , background='blue')
+    button_frame2.place(y=100, x=290)
+
     #       Create buttons
+
     global cancel_button, chooseGuide_button, saveGuide_button, generateHashAndSave_button, \
         doAlteration_button, data_alteration_check_button, value_alteration_check_button
 
-    #   Button block
-    button_frame = ttk.Frame(window, background='blue')
-    button_frame.pack(side='bottom', pady=20, padx=20)
+    # Save button
+    saveGuide_button = ttk.Button(button_frame2, text='Salvar guia', command=lambda: saveGuide())
+    saveGuide_button.pack(side='top', pady=10, padx=10)
+    saveGuide_button['state'] = 'disabled'
 
     # Choose button
-    chooseGuide_button = ttk.Button(button_frame, text='Carregar guia', command=lambda: chooseGuide())
-    chooseGuide_button.pack(side='right', pady=10, padx=10)
+    chooseGuide_button = ttk.Button(button_frame2, text='Carregar guia', command=lambda: chooseGuide())
+    chooseGuide_button.pack(side='top', pady=10, padx=10)
 
     # Cancel button
-    cancel_button = ttk.Button(button_frame, text='Cancelar', command=lambda: cancel())
-    cancel_button.pack(side='right', pady=10, padx=10)
+    cancel_button = ttk.Button(button_frame2, text='Cancelar', command=lambda: cancel())
+    cancel_button.pack(side='bottom', pady=10, padx=10)
     cancel_button['state'] = 'disabled'
 
     # Alteration button
     doAlteration_button = ttk.Button(button_frame, text='Realizar alterações', command=lambda: doAlteration())
-    doAlteration_button.pack(side='top', pady=10, padx=10)
+    doAlteration_button.pack(side='right', pady=10, padx=10)
     doAlteration_button['state'] = 'disabled'
 
     # Alterations modes
     data_alteration_check_button = ttk.Checkbutton(window, text='Alteração de dados', variable="data_alteration_check")
-    data_alteration_check_button.place(x=150, y=30)
+    data_alteration_check_button.place(x=260, y=30)
     data_alteration_check_button['state'] = 'disabled'
 
     value_alteration_check_button = ttk.Checkbutton(window, text='Alteração de valores',
                                                     variable="value_alteration_check")
-    value_alteration_check_button.place(x=150, y=50)
+    value_alteration_check_button.place(x=260, y=50)
     value_alteration_check_button['state'] = 'disabled'
-
-    # Save button
-    saveGuide_button = ttk.Button(button_frame, text='Salvar guia', command=lambda: saveGuide())
-    saveGuide_button.pack(side='right', pady=10, padx=10)
-    saveGuide_button['state'] = 'disabled'
 
     # Generate hash button
     generateHashAndSave_button = ttk.Button(button_frame, text='Gerar hash', command=lambda: saveGuide())
-    generateHashAndSave_button.pack(side='top', pady=10, padx=10)
+    generateHashAndSave_button.pack(side='left', pady=10, padx=0)
     generateHashAndSave_button['state'] = 'disabled'
 
+    openPlan_button = ttk.Button(window, text='Abrir planilha de\n alterações', command=lambda: openPlan())
+    openPlan_button.place(y=10, x=2)
 
-def disableButtons(button_list):
-    for button in button_list:
-        button['state'] = 'disabled'
-
-
-def enableButtons(button_list):
-    for button in button_list:
-        button['state'] = 'active'
+    return window
 
 
-def waitingOperation():
-    chooseGuide_button['state'] = 'disabled'
-    button_list = [doAlteration_button, generateHashAndSave_button, cancel_button,
-                   value_alteration_check_button, data_alteration_check_button]
-    enableButtons(button_list)
-
-
-def cancel():
-    chooseGuide_button['state'] = 'active'
-    button_list = [generateHashAndSave_button, doAlteration_button, cancel_button,
-                   data_alteration_check_button, value_alteration_check_button]
-    data_alteration_check_button.deselect(), value_alteration_check_button.deselect()
-    disableButtons(button_list)
-
+########################################################################################################################
 
 def chooseGuide():
+    def waitingOperation():
+        chooseGuide_button['state'] = 'disabled'
+        button_list = [doAlteration_button, generateHashAndSave_button, cancel_button,
+                       value_alteration_check_button, data_alteration_check_button]
+        enableButtons(button_list)
+
     global guide_path, tiss_guide, root_tag
     file_type = (('XML files', '*.xml'), ('All files', '*.*'))
     guide_path = fd.askopenfilename(filetypes=file_type)
@@ -244,9 +243,40 @@ def chooseGuide():
         mb.showwarning(title='Erro', message='A guia não foi escolhida!')
 
 
+
+########################################################################################################################
+
+def disableButtons(button_list):
+    for button in button_list:
+        button['state'] = 'disabled'
+
+
+########################################################################################################################
+
+def enableButtons(button_list):
+    for button in button_list:
+        button['state'] = 'active'
+
+
+########################################################################################################################
+
+def cancel():
+    chooseGuide_button['state'] = 'active'
+    button_list = [generateHashAndSave_button, doAlteration_button, cancel_button,
+                   data_alteration_check_button, value_alteration_check_button]
+    data_alteration_check_button.deselect(), value_alteration_check_button.deselect()
+    disableButtons(button_list)
+
+
+########################################################################################################################
+
 def doAlteration():
     data_alteration_check = data_alteration_check_button.getvar('data_alteration_check')
     value_alteration_check = value_alteration_check_button.getvar('value_alteration_check')
+
+    source_folder_path = 'sources'
+    p = source_folder_path
+    reviews_list = []
 
     def doDataAlteration():
         if data_alteration_check == '1':
@@ -307,9 +337,13 @@ def doAlteration():
                             reviews_list, 'values')
                         reviews_list.clear()
                         accounts = root_tag.iter('{http://www.ans.gov.br/padroes/tiss/schemas}guiaSP-SADT')
+
+                        if accounts is None:
+                            accounts = root_tag.iter('{http://www.ans.gov.br/padroes/tiss/schemas}guiaResumoInternacao')
+
                         for account in accounts:
-                            global cnt
-                            cnt = account
+                            global acnt
+                            acnt = account
                             if account_number != '':
                                 account_procedures = searchAccountProcedures(account)
                                 procedure_data = searchProcedureDataInProcedure(account_procedures)
@@ -341,9 +375,13 @@ def doAlteration():
         mb.showwarning('Erro', 'Escolha o modo de alteração')
 
 
-# Create GUI
-window = ttk.Tk()
-windowConfigure()
-createButtons()
+########################################################################################################################
 
-window.mainloop()
+def openPlan():
+    os.system(command=os.PathLike())
+
+
+########################################################################################################################
+
+GUI = createGui()
+GUI.mainloop()
