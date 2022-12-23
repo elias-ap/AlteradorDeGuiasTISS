773d558a79cf6508702fdc7527013ed48d4d76f2
Elias Araujo Pedroso <elias.ap.pro@gmail.com>
2022-12-23 16:25:23 -0300

    Commit changes

--- automatic_alteration.py
+++ automatic_alteration.py
@@ -13,7 +13,7 @@ from Exceptions import Exceptions
 
 def closeApplicationIfExecutionPathLocalIsNotOriginalDirPath():
     # right_execution_path = r"O:\Informatica\Geral\Funcionais\Faturamento de Convênios\Alterador de Guias TISS"
-    right_execution_path = r"C:\Users\elias\Documents\GitHub\python-automatics-data-alterations-in-xml-file"
+    right_execution_path = r"C:\Users\eliasp\Documents\GitHub\python-automatics-data-alterations-in-xml-file"
     current_execution_path = os.getcwd()
     if current_execution_path != right_execution_path:
         mb.showwarning('Erro', f'A aplicação só pode ser executada a partir do diretório original:\n{right_execution_path}')
@@ -100,6 +100,7 @@ def recalculateAllTotalValues(difference):
     elif guide_type == 'HOSPITALIZATION':
         account_total_values_tag = guide_accounts[0].find('ans:valorTotal', ans_prefix)
         general_total_values_tag = account_total_values_tag.find('ans:valorTotalGeral', ans_prefix)
+
     for total_value in account_total_values_tag:
         # IF TOTAL VALUE LARGER THAN DIFFERENCE VALUE ALTER
         if float(total_value.text) > float(difference):
@@ -405,7 +406,7 @@ def doValueAlteration(guide_accounts):
 
 
 def doAlterationAction():
-    global control_var
+    global control_var, guide_accounts
     data_alteration_check = data_alteration_check_button.get()
     value_alteration_check = value_alteration_check_button.get()
     guide_accounts = getGuideType()
